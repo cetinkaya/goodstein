2be477eeab41fc21e1bceb52e083803e7685814c
Ahmet Cetinkaya <cetinkayaahmet@yahoo.com>
2014-11-12 15:15:06 +0900

    Change type from Int to Integer.

--- goodstein.hs
+++ goodstein.hs
@@ -1,17 +1,17 @@
 -- 2014 - Ahmet Cetinkaya
 
-increaseBase :: Int -> Int -> Int
+increaseBase :: Integer -> Integer -> Integer
 increaseBase number base = increaseBase' number base 0 0
     where increaseBase' number base i acc
                             | number == 0 = acc
                             | otherwise = increaseBase' (div number base) base (i + 1) acc + ((base + 1) ^ (increaseBase i base)) * (rem number base)
 
 
-goodsteinIterate :: Int -> Int -> Int
+goodsteinIterate :: Integer -> Integer -> Integer
 goodsteinIterate number base = (increaseBase number base) - 1
 
 
-goodsteinSequence :: Int -> Int -> [Int]
+goodsteinSequence :: Integer -> Integer -> [Integer]
 goodsteinSequence initialNumber initialBase
   | initialNumber == 0 = [0]
   | otherwise = initialNumber : (goodsteinSequence (goodsteinIterate initialNumber initialBase) (initialBase + 1))
