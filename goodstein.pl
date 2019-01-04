add84ff2352183b4ffec8e07305f8914ef5ae345
Ahmet Cetinkaya <cetinkayaahmet@yahoo.com>
2019-01-05 00:47:00 +0900

    Change to snake case.

--- goodstein.pl
+++ goodstein.pl
@@ -1,19 +1,19 @@
 %% 2019 - Ahmet Cetinkaya
 
-increaseBaseAux(0, _, _, Acc, NewNumber) :- NewNumber is Acc.
-increaseBaseAux(Number, Base, Index, Acc, NewNumber) :- A is div(Number, Base),
-                                                        NextIndex is Index + 1,
-                                                        increaseBaseAux(Index, Base, 0, 0, Value),
-                                                        NewAcc is Acc + ((Base + 1) ** Value) * (Number rem Base),
-                                                        increaseBaseAux(A, Base, NextIndex, NewAcc, NewNumber).
+increase_base_aux(0, _, _, Acc, NewNumber) :- NewNumber is Acc.
+increase_base_aux(Number, Base, Index, Acc, NewNumber) :- A is div(Number, Base),
+                                                          NextIndex is Index + 1,
+                                                          increase_base_aux(Index, Base, 0, 0, Value),
+                                                          NewAcc is Acc + ((Base + 1) ** Value) * (Number rem Base),
+                                                          increase_base_aux(A, Base, NextIndex, NewAcc, NewNumber).
 
-increaseBase(Number, Base, NewNumber) :- increaseBaseAux(Number, Base, 0, 0, NewNumber).
+increase_base(Number, Base, NewNumber) :- increase_base_aux(Number, Base, 0, 0, NewNumber).
 
-goodsteinIterate(Number, Base, NextNumber) :- increaseBase(Number, Base, NewNumber), NextNumber is NewNumber - 1.
+goodstein_iterate(Number, Base, NextNumber) :- increase_base(Number, Base, NewNumber), NextNumber is NewNumber - 1.
 
-goodsteinSequence(0, _, [0]).
-goodsteinSequence(InitialNumber, InitialBase, [H|T]) :- InitialNumber > 0,
-                                                        H is InitialNumber,
-                                                        goodsteinIterate(InitialNumber, InitialBase, NextNumber),
-                                                        NextBase is InitialBase + 1,
-                                                        goodsteinSequence(NextNumber, NextBase, T).
+goodstein_sequence(0, _, [0]).
+goodstein_sequence(InitialNumber, InitialBase, [H|T]) :- InitialNumber > 0,
+                                                         H is InitialNumber,
+                                                         goodstein_iterate(InitialNumber, InitialBase, NextNumber),
+                                                         NextBase is InitialBase + 1,
+                                                         goodstein_sequence(NextNumber, NextBase, T).
